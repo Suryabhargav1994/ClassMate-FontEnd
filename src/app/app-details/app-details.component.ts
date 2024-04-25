6eb3de0c21e25b95e95b7f421fb67b04f5c021ba
Suryabhargav1994 <166631975+Suryabhargav1994@users.noreply.github.com>
2024-04-25 17:54:57 +0530

    Add files via upload

--- src/app/app-details/app-details.component.ts
+++ src/app/app-details/app-details.component.ts
@@ -1,60 +1,131 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ApiService } from '../api.service';
-import { NgxUiLoaderService } from 'ngx-ui-loader';
-import { AccomadationComponent } from '../accomadation/accomadation.component';
+import { Component, HostListener, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ApiService } from "../api.service";
+import { NgxUiLoaderService } from "ngx-ui-loader";
 
 @Component({
-  selector: 'app-app-details',
-  templateUrl: './app-details.component.html',
-  styleUrls: ['./app-details.component.scss']
+  selector: "app-app-details",
+  templateUrl: "./app-details.component.html",
+  styleUrls: ["./app-details.component.scss"],
 })
 export class AppDetailsComponent implements OnInit {
   universityId!: string;
-  accomadationArray: any;
+  accomadationArray: any[] = [];
   showBooked: boolean = false;
-  constructor(private route: ActivatedRoute,private apiservice: ApiService,private ngxService: NgxUiLoaderService,
-    private router:Router) { }
+  searchQuery: string = "";
+  distanceQuery: string = "";
+  showMenuBox: boolean = false;
+  user: any = {};
+  constructor(
+    private route: ActivatedRoute,
+    private apiservice: ApiService,
+    private ngxService: NgxUiLoaderService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      this.universityId = params['universityId'];
-      console.log(this.universityId);
-      this.loadAccList(this.universityId)
-      
-  });
+    this.route.queryParams.subscribe((params) => {
+      this.universityId = params["universityId"];
+      this.loadAccList(this.universityId);
+      this.applySearchFilter();
+      this.applyDistanceFilter();
+      this.getUserDetails();
+    });
   }
-  
-loadAccList(uni_id:any){
-  this.apiservice.listOfAccom(uni_id).subscribe((result:any) => {
-    this.accomadationArray = result.data;
-  });
-}
 
-
-bookedHouse(acc_id: any){
-  this.apiservice.booking(acc_id).subscribe((result: any) => {
-    console.log(result);
-    if(result && result.status === "200"){
-    this.showBooked = true;
+  loadAccList(uni_id: any) {
+    this.apiservice.listOfAccom(uni_id).subscribe((result: any) => {
+      this.accomadationArray = result.data.map((accommodation: any) => ({
+        ...accommodation,
+        booked: false,
+      }));
+      console.log(this.accomadationArray);
+    });
   }
-  })
-}
 
-redirectAminity(){
-  this.router.navigate(["aminity"])
-}
+  applySearchFilter(): void {
+    if (this.searchQuery.trim() === "") {
+      this.loadAccList(this.universityId);
+    } else {
+      this.accomadationArray = this.accomadationArray.filter(
+        (accommodation: any) =>
+          accommodation.accomidation_name
+            .toLowerCase()
+            .includes(this.searchQuery.toLowerCase())
+      );
+    }
+  }
 
-navigateUrl() {
-  this.router.navigate(["login"])
-}
+  applyDistanceFilter(): void {
+    if (this.distanceQuery.trim() === "") {
+      this.loadAccList(this.universityId);
+    } else {
+      this.accomadationArray = this.accomadationArray.filter(
+        (accommodation: any) =>
+          accommodation.distance
+            .toLowerCase()
+            .includes(this.distanceQuery.toLowerCase())
+      );
+    }
+  }
 
-navigateUrlTo() {
-  this.router.navigate(["uni-list"])
-}
+  bookedHouse(acc_id: any) {
+    this.apiservice.booking(acc_id).subscribe((result: any) => {
+      if (result && result.status === "200") {
+        const accommodation = this.accomadationArray.find(
+          (acc) => acc.accomidation_id === acc_id.accomidation_id
+        );
+        if (accommodation) {
+          accommodation.booked = true;
+        }
+      }
+    });
+  }
 
-accPopUp(){
-this.router.navigate(["accomdation"], { queryParams: { universityId: this.universityId } });
-}
+  toggleMenuBox() {
+    this.showMenuBox = !this.showMenuBox;
+  }
 
+  getUserDetails() {
+    const personId = localStorage.getItem("UserId");
+    this.apiservice.getUserDetails(personId).subscribe(
+      (response: any) => {
+        this.user = response.data[0];
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  deleteAccommodation(accommodation: any) {
+    this.apiservice
+      .deleteAccommodation(accommodation.accomidation_id)
+      .subscribe(
+        (response: any) => {
+          this.loadAccList(this.universityId);
+        },
+        (error: any) => {
+          console.error("Error deleting accommodation:", error);
+        }
+      );
+  }
+
+  redirectAminity() {
+    this.router.navigate(["aminity"]);
+  }
+
+  navigateUrl() {
+    this.router.navigate(["login"]);
+  }
+
+  navigateUrlTo() {
+    this.router.navigate(["uni-list"]);
+  }
+
+  accPopUp() {
+    this.router.navigate(["accomdation"], {
+      queryParams: { universityId: this.universityId },
+    });
+  }
 }
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,53 +1,50 @@
-import { Component, OnInit, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from "@angular/core";
 import {
   AbstractControl,
   FormBuilder,
   FormGroup,
   FormControl,
   Validators,
-} from '@angular/forms';
-import Validation from './utlis/validation';
-import { ApiService } from '../api.service';
-import { NgxUiLoaderService } from 'ngx-ui-loader';
-import { Router } from '@angular/router';
-import { LoaderService } from '../loader.service';
-
-
+} from "@angular/forms";
+import Validation from "./utlis/validation";
+import { ApiService } from "../api.service";
+import { NgxUiLoaderService } from "ngx-ui-loader";
+import { Router } from "@angular/router";
+import { LoaderService } from "../loader.service";
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  selector: "app-login-page",
+  templateUrl: "./login-page.component.html",
+  styleUrls: ["./login-page.component.scss"],
 })
 export class LoginPageComponent implements OnInit {
   form: FormGroup = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl(""),
+    password: new FormControl(""),
   });
   submitted = false;
   passwordHidden = true;
   emailexists = false;
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: FormBuilder,
     private apiservice: ApiService,
     private ngxService: NgxUiLoaderService,
-    private router:Router,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.form = this.formBuilder.group(
-      {
-        email: ['', [Validators.required, Validators.email]],
-        password: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(3),
-            Validators.maxLength(40),
-          ],
+    this.form = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(40),
         ],
-      },
-    );
+      ],
+    });
   }
 
   get f(): { [key: string]: AbstractControl } {
@@ -56,26 +53,24 @@ export class LoginPageComponent implements OnInit {
 
   onSubmit(): void {
     this.submitted = true;
-    if(this.form.valid){
-      debugger
+    if (this.form.valid) {
       this.ngxService.start();
-      this.apiservice.login(this.form.value).subscribe((result: any)=> {
-        if(result && result.status ===  "200"){
-          this.emailexists = false;
-          localStorage.setItem("UserId",result.data.user_id);
-          setTimeout(() => {
-            this.router.navigate(["uni-list"])
-          },2000)
-        }
-        else {
-          if(result.status === "401"){
-            this.emailexists = true;
+      this.apiservice.login(this.form.value).subscribe(
+        (result: any) => {
+          if (result && result.status === "200") {
+            this.emailexists = false;
+            localStorage.setItem("UserId", result.data.user_id);
+            setTimeout(() => {
+              this.router.navigate(["uni-list"]);
+            }, 2000);
+          } else {
+            if (result.status === "401") {
+              this.emailexists = true;
+            }
           }
-        }
-      },(err) => {
-        // this.ngxService.stop();
-        // this.toastr.error(err.error.message);
-      })
+        },
+        (err) => {}
+      );
     }
   }
 
